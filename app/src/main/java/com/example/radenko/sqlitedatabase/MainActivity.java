80bcd0354006dd17d96f4bf785be4fc8d1993ead
Radenko Kokanovic <radenko.kokanovic@gmail.com>
2018-01-31 21:37:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/radenko/sqlitedatabase/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.radenko.sqlitedatabase;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SQLiteDatabase mydatabase=openOrCreateDatabase("TestDB",MODE_PRIVATE,null);;
+
+        mydatabase.execSQL("DROP TABLE IF EXISTS TutorialsPoint;");
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS TutorialsPoint(Username VARCHAR,Password VARCHAR);");
+        mydatabase.execSQL("INSERT INTO TutorialsPoint VALUES('admin','admin');");
+        mydatabase.execSQL("INSERT INTO TutorialsPoint VALUES('radenko','kokanovic');");
+        mydatabase.execSQL("INSERT INTO TutorialsPoint VALUES('slavko','kokanovic');");
+        mydatabase.execSQL("INSERT INTO TutorialsPoint VALUES('test','module');");
+
+        Cursor resultSet = mydatabase.rawQuery("Select * from TutorialsPoint",null);
+       // resultSet.moveToFirst();
+//        String username = resultSet.getString(0);
+//        String password = resultSet.getString(1);
+//        int ime=resultSet.getColumnCount();
+//
+//        Log.i("mojLog",username);
+//        Log.i("MojLog",password);
+//        Log.i("MojLog",Integer.toString(ime));
+        while(resultSet.moveToNext()) {
+
+            Log.i("password", resultSet.getString(1));
+
+            Log.i("Username ",resultSet.getString(0));
+        }
+
+    }
+}
